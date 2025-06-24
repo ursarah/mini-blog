c1cdc6b89bfc36cafd40d6e214520518a974f888
ursarah <sarahurcezino@gmail.com>
2025-06-24 11:43:22 -0300

    feat: tirando exemplos no codigo

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -10,46 +10,11 @@ const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const [newPost, setNewPost] = useState([
-    {
-      id: 1,
-      titulo: "fgwer",
-      descricao: "sfadsf",
-      tags: "fawef,afwed",
-      url: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/945.png"
-    }, {
-      id: 2,
-      titulo: "sagasd",
-      descricao: "sfadsf",
-      tags: "fawef,afwed",
-      url: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/351.png"
-    }, {
-      id: 3,
-      titulo: "hjert",
-      descricao: "sfadsf",
-      tags: "fawef,afwed",
-      url: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/465.png"
-    }, {
-      id: 4,
-      titulo: "bsfgr",
-      descricao: "sfadsf",
-      tags: "fawef,afwed",
-      url: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/672.png"
-    }, {
-      id: 5,
-      titulo: "rgfsfbvs",
-      descricao: "sfadsf",
-      tags: "fawef,afwed",
-      url: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/532.png"
-    }, {
-      id: 6,
-      titulo: "gerfgerq",
-      descricao: "sfadsf",
-      tags: "fawef,afwed",
-      url: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/729.png"
-    },
   ])
+
   const [user, setUser] = useState(false);
   const { auth } = useAuth();
+
   const handleRemove = (id) => {
     setNewPost(newPost.filter((post) => post.id !== id))
     console.log(newPost);
